bb4c3b79bc87462ee86fda24414f24d2e42c6dee
Martaalex <marta.aleksandrovic@gmail.com>
2019-03-07 19:56:13 +0200

    timer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 10,
+    };
+    this.interval = null;
+  }
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState(
+        state => ({time: state.time - 1}),
+        () => {
+          if (this.state.time === 0) {
+            clearInterval(this.interval);
+          }
+        },
+      );
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>{this.state.time}</p>
           <a
             className="App-link"
             href="https://reactjs.org"
